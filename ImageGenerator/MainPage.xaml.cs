61806ca56e29682d0e3dc00920592a816da1a2b7
Sigge Millrud <sigge.millrud@learnet.se>
2025-10-22 19:02:33 +0200

    Lade till en stack-funktion

--- ImageGenerator/MainPage.xaml.cs
+++ ImageGenerator/MainPage.xaml.cs
@@ -6,23 +6,42 @@ namespace ImageGenerator
 {
     public partial class MainPage : ContentPage
     {
-        static private bool _isFavorite;
+        private class  ImageItem
+        {
+            public string ImageName { get; set; }
+            public string ImageDescription { get; set; }
+            public bool IsFavorite { get; set; }
+        }
 
-        private Dictionary<string, string> ImageList = new()
+        private readonly List<ImageItem> _images=new()
             {
-                {"image1", "Man" },
-                {"image2", "Bird" },
-                {"image3", "Big cat" },
-                {"image4", "Autumn road" },
-                {"image5", "Flowergirl" }
+                new ImageItem { ImageName = "image1", ImageDescription = "Man" },
+                new ImageItem { ImageName = "image2", ImageDescription = "Bird" },
+                new ImageItem { ImageName = "image3", ImageDescription = "Big cat" },
+                new ImageItem { ImageName = "image4", ImageDescription = "Autumn road" },
+                new ImageItem { ImageName = "image5", ImageDescription = "Flowergirl" },
+                new ImageItem { ImageName = "image6", ImageDescription = "Two small cats" },
+                new ImageItem { ImageName = "image7", ImageDescription = "Cow" },
+                new ImageItem { ImageName = "image8", ImageDescription = "Dog" },
+                new ImageItem { ImageName = "image9", ImageDescription = "Palm tree" },
+                new ImageItem { ImageName = "image10", ImageDescription = "City" }
+
+
+
+
             };
 
+        private ImageItem _currentImage;
 
         private Random random = new();
+
+        private Stack<ImageItem> _favoriteStack = new();
+
 
         public MainPage()
         {
             InitializeComponent();
+            ShowImageAndText();
         }
 
         private void ImageOnClicked(object? sender, EventArgs e)
@@ -32,18 +51,16 @@ namespace ImageGenerator
 
         private void ShowImageAndText()
         {
+            int randomIndex = random.Next(0 , _images.Count);
+            _currentImage = _images[randomIndex];
 
-            //var singleKeys = ImageList.Keys.ToList(); // en lokal lista av det första paret i en Dictionary
+            string imageName = GetImageFileEnding(_currentImage.ImageName);
 
-            var pairs = ImageList.ElementAt(random.Next(ImageList.Count));
+            ShowGallery.Source = imageName;
+            ImageText.Text = _currentImage.ImageDescription;
 
-            Debug.WriteLine(pairs.Key + ": " + pairs.Value); // för testning i Output
+            UpdateFavoriteButtonVisuals();
 
-            string showKey = GetImageFileEnding(pairs.Key); // detta då enbart Windows kräver filändelse
-
-            ShowGallery.Source = showKey;
-
-            ImageText.Text = pairs.Value;
         }
 
         private string GetImageFileEnding(string imageKey)
@@ -58,34 +75,64 @@ namespace ImageGenerator
 
         private void OnFavoriteClicked(object sender, EventArgs e)
         {
-            _isFavorite = !_isFavorite;
+            if (_currentImage == null) return;
 
-            if (_isFavorite)
+            if (_currentImage.IsFavorite)
             {
-                FavoriteButton.Source = new FontImageSource
-                {
-                    Glyph = "\ue87d",
-                    FontFamily = "MaterialIcons",
-                    Size = 32,
-                    Color = Colors.Red
-                };
+                _favoriteStack.Push(_currentImage);
             }
-            else
+
+            _currentImage.IsFavorite = !_currentImage.IsFavorite;
+
+            UpdateFavoriteButtonVisuals();
+
+
+            }
+
+
+        private void OnShowLastFavoriteClicked(object sender, EventArgs e)
+        {
+        if (_favoriteStack.Count>0)
             {
-                FavoriteButton.Source = new FontImageSource
-                {
-                    Glyph = "\ue87e",
-                    FontFamily = "MaterialIcons",
-                    Size = 32,
-                    Color = Colors.Gray
-                };
+                ImageItem lastFavorite = _favoriteStack.Pop();
+
+                _currentImage = lastFavorite;
+
+                ShowGallery.Source = GetImageFileEnding(_currentImage.ImageName) ;
+                ImageText.Text = _currentImage.ImageDescription ;
+
+                UpdateFavoriteButtonVisuals();
             }
         }
 
-        //public event PropertyChangedEventHandler PropertyChanged;
 
-        //protected void OnPropertyChanged([CallerMemberName] string name = null)
-        //    => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        private void UpdateFavoriteButtonVisuals()
+        {
+            if (_currentImage != null )
+            {
+                if (_currentImage.IsFavorite)
+                {
+                    FavoriteButton.Source = new FontImageSource
+                    {
+                        Glyph = "\ue87d",
+                        FontFamily = "MaterialIcons",
+                        Size = 32,
+                        Color = Colors.Red
+                    };
+                }
+                else
+                {
+                    FavoriteButton.Source = new FontImageSource
+                    {
+                        Glyph = "\ue87e",
+                        FontFamily = "MaterialIcons",
+                        Size = 32,
+                        Color = Colors.Gray
+                    };
+                }
+        }
+
+        }
 
     }
 }
